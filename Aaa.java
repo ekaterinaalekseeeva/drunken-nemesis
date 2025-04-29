5ea421390ffbb4629da8662b353cb0d8d147f04d
ubuntu <alekseevaekaterinaa@gmail.com>
2025-04-29 14:03:17 +0000

    so many changes

--- Aaa.java
+++ Aaa.java
@@ -2,6 +2,6 @@ class Aaa {
  public static void main (String[] args) {
   System.out.println("hello world");
   System.out.println("sfl");
-  System.out.println("sfl");
+  System.out.println("ololol");
  }
 }
