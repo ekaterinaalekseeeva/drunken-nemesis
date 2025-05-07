5d9e2b10d06c2c1ae7bf5117ca40f1e887d5401a
ubuntu <alekseevaekaterinaa@gmail.com>
2025-05-07 16:55:08 +0000

    bbb

--- Bbb.java
+++ Bbb.java
@@ -1,8 +1,10 @@
 public class Bbb {
     public static void main(String[] args) {
         System.out.println("hello world!");
+        //aa
         System.out.println("hello world!!");
         System.out.println("hello world!!!");
         System.out.println("hello world!!!!");
+        //bb
     }
 }
