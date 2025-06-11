f3a00fc05f97b6b8b68d7f0dc7b37df14c60cb3e
ubuntu <alekseevaekaterinaa@gmail.com>
2025-06-11 21:31:14 +0000

    changes

--- Aaa.java
+++ Aaa.java
@@ -1,7 +1,11 @@
 class Aaa {
  public static void main (String[] args) {
+  //hi
   System.out.println("hello world");
+  //i'm here
   System.out.println("sfl");
+  //to add some
   System.out.println("ololol");
+  //comments
  }
 }
--- Bbb.java
+++ Bbb.java
@@ -1,11 +1,18 @@
 public class Bbb {
     public static void main(String[] args) {
+        // some comment here
         System.out.println("hello world!");
         //aa
         System.out.println("hello world!!");
         System.out.println("hello world!!!");
         System.out.println("hello world!!!!");
         //bb
+        //here
         //cc
+        //and here
+    }
+
+    public int foo(){
+        return  0;
     }
 }
